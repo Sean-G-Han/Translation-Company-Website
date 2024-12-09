9321197cd4056ccdc324f94bca6594aba8726769
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2024-12-09 15:45:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const http = require('http');
 http.createServer(dataControl).listen(4500);
 
 function dataControl(req, res) {
-    res.write("<h1>Tan Yi Han</h1>");
+    res.write("<h1>Hi I am Tan Yi Han</h1>");
     res.end();
 }
